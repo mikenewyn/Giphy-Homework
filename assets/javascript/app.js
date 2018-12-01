ee390291f60454340d6af0438676257bd59faa17
Thumper <thinknownotforever@gmail.com>
2018-11-30 21:10:26 -0800

    Got the ajax to query!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,32 @@
-var marvel = ["spiderman", "ironman", "black panther", "dr. strange", "hulk", "wolverine", "cyclops"]
+var topics = ["Spiderman", "Ironman", "Black Panther", "Dr. Strange", "Hulk", "Wolverine", "Cyclops"];
 
-var key = "SsRcRExbSi0dtRlOTjRGL7zpxiCZNIPR"
+function newButton() {
+  $("#button-area").empty();
+  for (var i = 0; i < topics.length; i++ ){
+    $("#button-area").append("<button id='char-button' char='" + topics[i] + "'>" + topics[i]);
+  }
+}
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=corgis&api_key=SsRcRExbSi0dtRlOTjRGL7zpxiCZNIPR";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    for(var i = 0; i < response.data.length; i++){
+$(document).on("click", "#char-button", function() {
+  var charInfo = $(this).attr("char");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + charInfo + "&api_key=SsRcRExbSi0dtRlOTjRGL7zpxiCZNIPR";
+  gifPull();
+  function gifPull(){
+    $("image-area").empty();
+    event.preventDefault();
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      for(var i = 0; i < response.data.length; i++){
         $("#gifs").append("<img src='" + response.data[i].images.downsized.url + "'/>");
       }
-  });
+      })
+  }
+});
+
+newButton();
+
+
+
